a0fdd672ffb323b3b60806ccab42fd555d85ba33
Chester Cari <chester_cari@outlook.com>
2023-04-01 01:31:01 -0400

    added performance monitor

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,6 @@
 import { Grid, OrbitControls } from "@react-three/drei";
 import { useControls } from "leva";
+import { Perf } from "r3f-perf";
 import React from "react";
 
 type Props = {};
@@ -8,6 +9,7 @@ const Experience = (props: Props) => {
   const { scale } = useControls({ scale: -2 });
   return (
     <>
+      <Perf position={"top-left"} />
       <OrbitControls />
       <Grid cellColor="white" args={[10, 10]} />
       <mesh scale={scale}>
